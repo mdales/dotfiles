42336351d52c61c3e15f9840aa1752843d3f9724
Michael Dales <michael@digitalflapjack.com>
2020-05-26 18:51:12 +0100

    added powerline-go

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
-set -x PATH /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin /usr/local/go/bin $HOME/go/bin
+set -x PATH /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin /usr/local/go/bin $HOME/go/bin $HOME/.cargo/bin
 
 set -x EDITOR vi
 
-function fish_prompt
+function old_fish_prompt
     set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
 
-    set_color green 
+    set_color green
     printf '%s' (hostname)
-    set_color B90    
+    set_color B90
     printf "[%s" (basename $PWD)
     if string length "$git_branch" > /dev/null
         printf ":$git_branch"
@@ -32,4 +32,13 @@ function ls --description 'List contents of directory'
         set param $param --indicator-style=classify
     end
     command ls $param $argv
+end
+
+function open
+    set -l wslpath (/bin/wslpath -w $argv)
+   /mnt/c/Windows/system32/cmd.exe /c start "" "$wslpath"
+end
+
+function fish_prompt
+    eval /home/michael/go/bin/powerline-go -error $status -shell bare -cwd-mode dironly -colorize-hostname -modules "nix-shell,venv,host,ssh,cwd,perms,gitlite,hg,exit,root,vgo"
 end
